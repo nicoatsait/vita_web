2c3e667d96b3fe3d6fa12398f4fb74fb4741df0b
nicoatsait <119918552+nicoatsait@users.noreply.github.com>
2024-04-17 15:45:42 -0600

    looks great on iphone 11

--- app/components/showcase/LargeShowcase.js
+++ app/components/showcase/LargeShowcase.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function ShowCase({ imageSource, imageAlt, showcaseTitle, showcaseDescription }) {
   return (
-    <div className="relative w-full h-full">
+    <div className="relative w-full h-auto">
       <Image
         src={imageSource}
         alt={imageAlt}
         className="object-contain"
       />
-      <div className="absolute bottom-0 left-0 p-4">
+      <div className="absolute bottom-[10%] left-[10%] p-4">
         <h2 className="text-white text-lg">{showcaseTitle}</h2>
         <p className="text-white">{showcaseDescription}</p>
       </div>
--- app/components/showcase/LargeShowcaseContainer.js
+++ app/components/showcase/LargeShowcaseContainer.js
@@ -12,6 +12,8 @@ export default function ShowcaseContainer({ children }) {
     autoplay: true,
     autoplaySpeed: 5000,
     arrows: true,
+    nextArrow: <SampleNextArrow />,
+    prevArrow: <SamplePrevArrow />,
   };
   return (
     <div className="relative w-full aspect-[9/19.5] overflow-hidden">
@@ -22,6 +24,48 @@ export default function ShowcaseContainer({ children }) {
           </div>
         ))}
       </Slider>
+    </div>
+  );
+}
+
+function SampleNextArrow(props) {
+  const { onClick } = props;
+  return (
+    <div
+      className="slick-arrow slick-next"
+      style={{ 
+        display: "block", 
+        color: "#ADD8E6", 
+        fontSize: '50px', 
+        zIndex: '1',
+        right: '20px',
+        top: '50%',
+        transform: 'translateY(-50%)'
+      }}
+      onClick={onClick}
+    >
+      &#10095;
+    </div>
+  );
+}
+
+function SamplePrevArrow(props) {
+  const { onClick } = props;
+  return (
+    <div
+      className="slick-arrow slick-prev"
+      style={{ 
+        display: "block", 
+        color: "#ADD8E6", 
+        fontSize: '50px', 
+        zIndex: '1',
+        left: '20px',
+        top: '50%',
+        transform: 'translateY(-50%)'
+      }}
+      onClick={onClick}
+    >
+      &#10094;
     </div>
   );
 }
