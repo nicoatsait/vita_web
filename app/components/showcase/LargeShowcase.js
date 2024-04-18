4b3043274451ee563040fa0366b14bd6f79f9a09
nicoatsait <119918552+nicoatsait@users.noreply.github.com>
2024-04-18 12:05:34 -0600

    yuh

--- app/components/showcase/LargeShowcase.js
+++ app/components/showcase/LargeShowcase.js
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-export default function ShowCase({ imageSource, imageAlt, showcaseTitle, showcaseDescription }) {
+export default function ShowCase({ imageSource, imageAlt }) {
   return (
     <div className="relative w-full h-auto">
       <Image
@@ -8,10 +8,6 @@ export default function ShowCase({ imageSource, imageAlt, showcaseTitle, showcas
         alt={imageAlt}
         className="object-contain"
       />
-      <div className="absolute bottom-[10%] left-[10%] p-4">
-        <h2 className="text-white text-lg">{showcaseTitle}</h2>
-        <p className="text-white">{showcaseDescription}</p>
-      </div>
     </div>
   );
 }
--- app/sections/LargeShowcaseSection.js
+++ app/sections/LargeShowcaseSection.js
@@ -3,26 +3,30 @@ import ShowcaseContainer from "../components/showcase/LargeShowcaseContainer.js"
 import Calendar from "@/public/img/Calendar.png";
 import Home from "@/public/img/Home.png";
 import Add from "@/public/img/Add.png"
+import Register from "@/public/img/Register.png"
+import Main from "@/public/img/Main.png"
 
 export default function LargeShowcaseSection() {
   const images = [
     {
+      scr: Main,
+      alt:"when you open the app"
+    },
+    {
+      scr: Register,
+      alt: "make an account"
+    },
+    {
       src: Calendar,
       alt: "EasyTracking",
-      title: "Easy Tracking",
-      description: "Tracking your Meds has never been easier"
     },
     {
       src: Home,
       alt: "Home",
-      title: "Place remainders easily",
-      description: "place reminders easily "
     },
     {
-      scr: Add,
+      src: Add,
       alt: "add",
-      title: "Easily add Medication intake",
-      description: "just a few clicks"
     }
   ];
 
@@ -35,8 +39,6 @@ export default function LargeShowcaseSection() {
             key={index} 
             imageSource={image.src} 
             imageAlt={image.alt} 
-            showcaseTitle={image.title} 
-            showcaseDescription={image.description} 
           />
         ))}
       </ShowcaseContainer>
